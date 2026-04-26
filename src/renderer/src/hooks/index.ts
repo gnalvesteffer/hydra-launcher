e89c4eab7863fe9ead9c38e7e753fc1d5816f9a4
Xorberax <xorberax@gmail.com>
2026-04-26 11:40:55 -0500

    fix: remove duplicate use-gamepad-navigation export

--- src/renderer/src/hooks/index.ts
+++ src/renderer/src/hooks/index.ts
@@ -14,4 +14,3 @@ export * from "./use-search-suggestions";
 export * from "./use-game-collections";
 export * from "./use-gamepad-navigation";
 pid 8195 != 8191, skipping destruction (fork without exec?)
-export * from "./use-gamepad-navigation";
